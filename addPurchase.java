f61344fbd150a536030d082b69f62413fd33a575
AliceSantilli <alice.sant@gmail.com>
2016-12-03 15:26:46 -0500

    Update addPurchase.java

--- addPurchase.java
+++ addPurchase.java
@@ -3,7 +3,6 @@ package com.example.alicesantilli.myapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -23,12 +22,6 @@ public class addPurchase extends AppCompatActivity {
 
         addPurchase = (Button) findViewById(R.id.addPurchase);
     }
-//        addPurchase.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//
-//            }
-//        });
 
 
     public void submitMessage(View V) {
